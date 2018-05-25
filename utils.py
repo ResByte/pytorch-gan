918a33e18995fc9bdd561878f969e1a5bc9bfd57
Abhinav Dadhich <dadhich@abeja.asia>
2018-05-25 16:55:21 +0900

    clean up code for dcgan and training style

---
+++ utils.py
@@ -0,0 +1,88 @@
+import argparse
+import random
+import torch
+import torch.backends.cudnn as cudnn
+import os
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+
+def config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw | fake')
+    parser.add_argument('--dataroot', required=True, help='path to dataset')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+    parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+    parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+    parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+    parser.add_argument('--ngf', type=int, default=64)
+    parser.add_argument('--ndf', type=int, default=64)
+    parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+    parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+    parser.add_argument('--cuda', action='store_true', help='enables cuda')
+    parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+    parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+    parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+    parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+    parser.add_argument('--manualSeed', type=int, help='manual seed')
+
+    opt = parser.parse_args()
+    try:
+        os.makedirs(opt.outf)
+    except OSError:
+        pass
+
+    if opt.manualSeed is None:
+        opt.manualSeed = random.randint(1, 10000)
+    print("Random Seed: ", opt.manualSeed)
+    random.seed(opt.manualSeed)
+    torch.manual_seed(opt.manualSeed)
+    if opt.cuda:
+        torch.cuda.manual_seed_all(opt.manualSeed)
+
+    cudnn.benchmark = True
+    if torch.cuda.is_available() and not opt.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    print(opt)
+    return opt
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+        
+def get_dataset(opt):
+    if opt.dataset in ['imagenet', 'folder', 'lfw']:
+        # folder dataset
+        dataset = dset.ImageFolder(root=opt.dataroot,
+                                   transform=transforms.Compose([
+                                       transforms.Scale(opt.imageSize),
+                                       transforms.CenterCrop(opt.imageSize),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                   ]))
+    elif opt.dataset == 'lsun':
+        dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
+                            transform=transforms.Compose([
+                                transforms.Scale(opt.imageSize),
+                                transforms.CenterCrop(opt.imageSize),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ]))
+    elif opt.dataset == 'cifar10':
+        dataset = dset.CIFAR10(root=opt.dataroot, download=True,
+                               transform=transforms.Compose([
+                                   transforms.Scale(opt.imageSize),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
+    elif opt.dataset == 'fake':
+        dataset = dset.FakeData(image_size=(3, opt.imageSize, opt.imageSize),
+                                transform=transforms.ToTensor())
+    assert dataset
+    return dataset
